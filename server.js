2b7e9115e9e8ff6269fe88d21aeba51315738f0e
hthmark <heretohelpmarketing@gmail.com>
2026-06-07 11:50:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
